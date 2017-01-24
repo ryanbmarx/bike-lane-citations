823ece50e94fb33d0e46d191b8a318fad4ec0440
Kyle Bentle <kylebentle@gmail.com>
2017-01-24 11:37:29 -0600

    Adds files for geocoding, including the most up to date, cleaned and geocoded datasets.

--- data/geocoding/geocoder.py
+++ data/geocoding/geocoder.py
@@ -3,16 +3,18 @@ import requests, re, time
 import sys
 import pandas as pd
 
-def get_latlong(address):
+def get_latlong(record):
   try:
-    url = 'http://dev.virtualearth.net/REST/v1/Locations?q='+address+'&key=AlAY-bnKDAZKs2G2IovGDZmM6RjLaT9eULsU0DhHd2pjh00rukW9otNQlqdmO1Pl'
+    url = 'http://dev.virtualearth.net/REST/v1/Locations?q='+record["location"]+'&key=AlAY-bnKDAZKs2G2IovGDZmM6RjLaT9eULsU0DhHd2pjh00rukW9otNQlqdmO1Pl'
     r = requests.get(url)
     answer = r.json()
     rsp = answer['resourceSets'][0]['resources'][0]['geocodePoints'][0]
     coors = rsp['coordinates']
-    print coors, ",", address
+    print coors, ",", record["location"],",",record["id"]
+    return coors
+    
   except requests.exceptions.ConnectionError:
-    evictions.to_csv('out.csv', encoding="utf-8")
+    citation.to_csv('failed-parking-on-bike-path.csv', encoding="utf-8")
     print('waiting...')
     time.sleep(5)
   except TypeError:
@@ -26,7 +28,7 @@ def get_latlong(address):
     print 'index error'
     return None
 
-evictions = pd.read_csv('in.csv')
-evictions['coors'] = evictions['address'].apply(get_latlong)
+citation = pd.read_csv('failed-parking-on-bike-path.csv')
+citation['coors'] = citation.apply(get_latlong, axis=1)
 
-evictions.to_csv('out.csv', encoding="utf-8")
+citation.to_csv('failed-parking-on-bike-path-locs.csv', encoding="utf-8")
